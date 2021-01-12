81cdc9f8ff511da7c8b9e028b4c86171c8a052e5
natcobbinah <nathaniel.cobbinah@amalitech.org>
2021-01-12 15:36:28 +0000

    test performed with enzyme library

--- hackernews/src/App.test.js
+++ hackernews/src/App.test.js
@@ -3,12 +3,16 @@ import App,{Search,Button,Table} from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
 
 /* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 }); */
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
 it('renders without crashing',() => {
@@ -78,4 +82,21 @@ describe('Table', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
+})
+
+describe('Table', () => {
+  const props = {
+    list: [
+      {title: '1', author: '1', num_comments: 1, points: 2, objectID:'y'},
+      {title: '2', author: '2', num_comments: 1, points: 2, objectID:'z'}
+    ]
+  }
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table {...props}/>
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
 })
